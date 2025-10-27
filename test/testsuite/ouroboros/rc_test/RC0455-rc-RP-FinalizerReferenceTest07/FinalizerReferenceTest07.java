c35d362d7266d4a47c97e8f33e614140f9cc55e3
SendaoYan <yansendao@hygon.cn>
2025-10-27 15:09:33 +0000

    static PhantomReference<ReferenceTest07> sr07 = new PhantomReference<>(null, referenceQueue);

--- test/testsuite/ouroboros/rc_test/RC0455-rc-RP-FinalizerReferenceTest07/FinalizerReferenceTest07.java
+++ test/testsuite/ouroboros/rc_test/RC0455-rc-RP-FinalizerReferenceTest07/FinalizerReferenceTest07.java
@@ -23,7 +23,7 @@ import java.lang.ref.ReferenceQueue;
 
 public class FinalizerReferenceTest07 {
     static ReferenceQueue referenceQueue = new ReferenceQueue();
-    static PhantomReference<ReferenceTest07> sr07;
+    static PhantomReference<ReferenceTest07> sr07 = new PhantomReference<>(null, referenceQueue);
     static int check = 2;
 
     public static void main(String[] args) {
